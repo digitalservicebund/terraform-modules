598d6f6764c167694831c7d5cfcfd515507e761e
Klaus Hartl <klaus.hartl@gmail.com>
2022-04-14 13:58:00 +0200

    Fix private deploy key format
    GitHub action runner needs openssh.

--- deploy-key/main.tf
+++ deploy-key/main.tf
@@ -6,7 +6,7 @@ resource "github_actions_environment_secret" "private_deploy_key" {
   repository      = var.deploying_repository
   environment     = var.environment
   secret_name     = var.deploying_repository_private_key_secret_name
-  plaintext_value = tls_private_key.deploy_key.private_key_pem
+  plaintext_value = tls_private_key.deploy_key.private_key_openssh
 }
 
 resource "github_repository_deploy_key" "public_deploy_key" {
