34399563063e14de3917e588e9fd51c9d8454829
Julian Klusmann-Rösner <julian.roesner@digitalservice.bund.de>
2023-03-13 12:21:26 +0100

    Adapt output

--- otc-rds-database/outputs.tf
+++ otc-rds-database/outputs.tf
@@ -5,6 +5,6 @@ output "address" {
 
 output "password" {
   description = "Database password"
-  value       = random_password.this.result
+  value       = opentelekomcloud_rds_instance_v3.this.db.password
   sensitive   = true
 }
