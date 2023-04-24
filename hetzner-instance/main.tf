88f0df1804fcb2b94556acdaecb2b4df4fe1469e
Christian Kaatz <christian.kaatz@digitalservice.bund.de>
2023-04-24 15:11:51 +0200

    Fix typo

--- hetzner-instance/main.tf
+++ hetzner-instance/main.tf
@@ -1,7 +1,7 @@
 resource "hcloud_server" "server" {
   name        = "${var.stack_name}"
   image       = "${var.image}"
-  server_type = "${var.instance_type}}"
+  server_type = "${var.instance_type}"
   datacenter  = "${var.datacenter}"
   ssh_keys    = var.ssh_key_ids
   user_data   = file("${var.userdata_path}")
