d407695f648ddc098c1b51ec76850305104237a7
Niko Felger <niko.felger@gmail.com>
2023-07-04 15:25:05 +0200

    repo-environment: set up branch protection

--- repo-environment/main.tf
+++ repo-environment/main.tf
@@ -1,4 +1,16 @@
 resource "github_repository_environment" "environment" {
   repository  = var.repository
   environment = var.environment
+
+  deployment_branch_policy {
+    protected_branches     = false
+    custom_branch_policies = true
+  }
+}
+
+resource "github_repository_deployment_branch_policy" "foo" {
+  reposistory      = var.repository
+  environment_name = var.environment
+
+  name = var.branch_name_pattern
 }
--- repo-environment/variables.tf
+++ repo-environment/variables.tf
@@ -1,2 +1,8 @@
 variable "environment" {}
 variable "repository" {}
+
+variable "branch_name_pattern" {
+  description = "The name pattern that branches must match in order to deploy to the environment."
+  type        = string
+  default     = "main"
+}
