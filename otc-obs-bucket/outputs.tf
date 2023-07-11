34b62678fabd8301fb34fbcdc8552c5db58c3724
Kai Bernhard <17817496+kaibernhard@users.noreply.github.com>
2023-07-11 16:13:16 +0200

    Provide kms_key_id in output of otc-obs-bucket module

--- otc-obs-bucket/outputs.tf
+++ otc-obs-bucket/outputs.tf
@@ -12,3 +12,8 @@ output "region" {
   description = "The region this bucket resides in."
   value       = opentelekomcloud_obs_bucket.this.region
 }
+
+output "kms_key_id" {
+  description = "The ID of the KMS key."
+  value       = opentelekomcloud_kms_key_v1.this.id
+}
