f03ad969678409030e7d02904fda9bf0779a27b2
Christian Kaatz <christian.kaatz@digitalservice.bund.de>
2023-10-11 09:17:35 +0200

    Add a firewall to each hetzner instance

--- hetzner-instance/main.tf
+++ hetzner-instance/main.tf
@@ -1,3 +1,37 @@
+resource "hcloud_firewall" "firewall" {
+  name = "${var.stack_name}-firewall"
+
+  rule {
+    direction = "in"
+    protocol  = "tcp"
+    port      = "80" //http
+    source_ips = [
+      "0.0.0.0/0",
+      "::/0"
+    ]
+  }
+  rule {
+    direction = "in"
+    protocol  = "tcp"
+    port      = "443" //https
+    source_ips = [
+      "0.0.0.0/0",
+      "::/0"
+    ]
+  }
+
+  rule {
+    direction = "in"
+    protocol  = "tcp"
+    port      = "22" //ssh
+    source_ips = [
+      "0.0.0.0/0",
+      "::/0"
+    ]
+  }
+
+}
+
 resource "hcloud_server" "server" {
   name        = var.stack_name
   image       = var.image
@@ -5,6 +39,7 @@ resource "hcloud_server" "server" {
   datacenter  = var.datacenter
   ssh_keys    = var.ssh_key_ids
   user_data   = file("${var.userdata_path}")
+  firewall_ids = [ hcloud_firewall.firewall.id ]
   public_net {
     ipv4_enabled = true
     ipv6_enabled = var.enable_ipv6
