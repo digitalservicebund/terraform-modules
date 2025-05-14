13779fbcf9375f28ff6913f0457e2b3dbdc9e722
Volker Schiewe <volker.schiewe-external@digitalservice.bund.de>
2025-05-14 11:45:01 +0200

    build(aws-remote-state): Add backend.tf output

---
+++ aws-remote-state/outputs.tf
@@ -0,0 +1,17 @@
+output "backend_tfile" {
+  value       = <<-EOT
+cat <<EOF > backend.tf
+terraform {
+  backend "s3" {
+    bucket         = "${aws_s3_bucket.terraform_state_bucket.bucket}"
+    kms_key_id     = "${aws_kms_key.terraform_s3_bucket_kms_key.arn}"
+    key            = "tfstate-backend"
+    region         = "${aws_s3_bucket.terraform_state_bucket.region}"
+    dynamodb_table = "${aws_dynamodb_table.terraform_lock_table.name}"
+    encrypt        = true
+  }
+}
+EOF
+    EOT
+  description = "The content of the backend.tf file that should be created to use this remote state"
+}
