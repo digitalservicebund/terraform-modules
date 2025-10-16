400fd0c744f1b9df723eebea10bfa7e987935bf5
Volker Hartmann <volker.hartmann-external@digitalservice.bund.de>
2025-10-16 10:27:51 +0200

    feat(stackit-state-bucket): Add character limit validation

--- stackit-state-bucket/variables.tf
+++ stackit-state-bucket/variables.tf
@@ -6,4 +6,9 @@ variable "project_id" {
 variable "state_bucket_name" {
   description = "The name of the state bucket."
   type        = string
+
+  validation {
+    condition     = length(var.state_bucket_name) <= 29
+    error_message = "The state_bucket_name must not exceed 29 characters, because the credentials group name has a limit of 32 characters."
+  }
 }
