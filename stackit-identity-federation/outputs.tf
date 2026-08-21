edf41b4a5329685b68c4fc941a078c5413921fe3
Volker Hartmann <volker.hartmann@digitalservice.bund.de>
2026-08-21 09:27:21 +0200

    feat(permissions): introduce custom role creation for least privilege access PL-241
    Second attempt, because release action has not been triggered in #107

--- stackit-identity-federation/outputs.tf
+++ stackit-identity-federation/outputs.tf
@@ -2,7 +2,6 @@ output "service_account_email" {
   value       = stackit_service_account.this.email
   description = "Email of the created service account"
 }
-
 output "custom_role_name" {
   value       = one(stackit_authorization_organization_custom_role.this[*].name)
   description = "Name of the custom role created from var.permissions, null if no permissions were supplied"
