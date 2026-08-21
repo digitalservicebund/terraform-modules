5ed5b2dfd2156d3a7771da176ab3bdaf484a076b
Volker Hartmann <volker.hartmann@digitalservice.bund.de>
2026-08-21 14:20:21 +0200

    fix(stackit-identity-federation): implement support for custom roles at project, folder, and organization levels

--- stackit-identity-federation/outputs.tf
+++ stackit-identity-federation/outputs.tf
@@ -3,6 +3,20 @@ output "service_account_email" {
   description = "Email of the created service account"
 }
 output "custom_role_name" {
-  value       = one(stackit_authorization_organization_custom_role.this[*].name)
+  value = one(concat(
+    stackit_authorization_project_custom_role.this[*].name,
+    stackit_authorization_folder_custom_role.this[*].name,
+    stackit_authorization_organization_custom_role.this[*].name,
+  ))
   description = "Name of the custom role created from var.permissions, null if no permissions were supplied"
+}
+
+output "role_scope" {
+  value       = local.role_scope
+  description = "The level (project, folder or organization) the custom role and role assignments are created on"
+}
+
+output "resource_id" {
+  value       = local.resource_id
+  description = "ID of the resource (project, folder or organization) the custom role and role assignments are created on"
 }
