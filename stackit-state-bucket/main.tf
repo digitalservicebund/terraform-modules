df9b2b0e225c165c5557065e0b655aedc1fb5338
Volker Hartmann <volker.hartmann@digitalservice.bund.de>
2026-08-31 14:34:49 +0200

    chore(stackit-state-bucket): adjust spacing in module source block for consistency

--- stackit-state-bucket/main.tf
+++ stackit-state-bucket/main.tf
@@ -1,5 +1,5 @@
 module "object_storage" {
-  source            = "github.com/digitalservicebund/terraform-modules?ref=stackit-object-storage/v1.0.3"
+  source                         = "github.com/digitalservicebund/terraform-modules?ref=stackit-object-storage/v1.0.3"
   project_id                     = var.project_id
   bucket_name                    = var.state_bucket_name
   credentials                    = {}   # No additional credentials needed
